a2d10c4fdeb3bb5f1b8b6aa12da043b082dc4030
CHOO CH <CHOO CH@LAPTOP-D9460APR>
2020-07-25 15:18:32 +0800

    Add in LEH

--- src/AssignmentMain.java
+++ src/AssignmentMain.java
@@ -3,7 +3,7 @@ public class AssignmentMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HELLO CANNOT");
+		System.out.println("HELLO CANNOT LEH");
 	}
 
 }
