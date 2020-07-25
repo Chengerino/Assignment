af80d133dfae876ceb1078042d2116e182eae6a2
Ka Kit <Ka Kit@DESKTOP-5P7C7C4>
2020-07-25 15:22:46 +0800

    hi

--- src/AssignmentMain.java
+++ src/AssignmentMain.java
@@ -4,6 +4,7 @@ public class AssignmentMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HELLO CANNOT LEH");
+		System.out.println("CARSONYOUDAMNSMARTWIEH");
 	}
 
 }
